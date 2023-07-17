3c499dfc07f9d0dc06ab055f9c39616741c97ec8
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-07-17 10:35:13 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "public_ip" {
-  value       = aws_instance.my_vm.public_ip
+  value       = aws_instance.ec2.public_ip
   description = "Public IP Address"
 }
 
 output "instance_id" {
-  value       = aws_instance.my_vm.id
+  value       = aws_instance.ec2.id
   description = "Instance ID"
 }
