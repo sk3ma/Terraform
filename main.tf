1384946d17299648f5ae9d750d304cb9feb7e51b
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-07-17 10:34:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,48 +12,47 @@ terraform {
 }
 
 provider "aws" {
-  shared_config_files      = ["/home/levon/.aws/config"]
-  shared_credentials_files = ["/home/levon/.aws//credentials"]
-  profile                  = "default"
-  region                   = "eu-west-1"
+  shared_credentials_file = "/home/levon/.aws/credentials"
+  profile                 = "default"
+  region                  = "eu-west-1"
 }
 
 /* AWS EC2 resource */
-resource "aws_instance" "terraformec2" {
-  ami                         = "var.ami_id"
-  count                       = "var.number_of_instances"
-  instance_type               = "var.instance_type"
+resource "aws_instance" "ec2" {
+  ami                         = var.ami_id
+  count                       = var.number_of_instances
+  instance_type               = var.instance_type
   key_name                    = "mykeypair"
-  user_data = <<STOP
-    #! /bin/bash
+  user_data = <<-EOF
+    #!/bin/bash
     sudo apt update
     sudo apt install apache2 -y
     sudo systemctl start apache2
     sudo systemctl enable apache2
     echo "<h1>Deployed using Terraform</h1>" > /var/www/html/index.html
-  STOP
-  subnet_id                   = aws_subnet.terraform_public_subnet.id
-  vpc_security_group_ids      = [aws_security_group.terraform_sg.id]
+  EOF
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
   tags = {
-      Name = "My_EC2"
-      Description = "EC2 virtual server"
-    }
-} 
+    Name        = "My_EC2"
+    Description = "EC2 virtual server"
+  }
+}
 
 /* AWS VPC resource */
-resource "aws_vpc" "terraform_vpc" {
+resource "aws_vpc" "ec2_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-      Name = "My_VPC"
-      Description = "EC2 custom VPC"
+    Name        = "My_VPC"
+    Description = "EC2 custom VPC"
   }
 }
 
 /* Defining network component */
-resource "aws_subnet" "terraform_public_subnet" {
-  vpc_id            = aws_vpc.terraform_vpc.id
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.ec2_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-west-1a"
 
@@ -62,8 +61,8 @@ resource "aws_subnet" "terraform_public_subnet" {
   }
 }
 
-resource "aws_subnet" "terraform_private_subnet" {
-  vpc_id            = aws_vpc.terraform_vpc.id
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.ec2_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-west-1a"
 
@@ -72,41 +71,42 @@ resource "aws_subnet" "terraform_private_subnet" {
   }
 }
 
-resource "aws_internet_gateway" "terraform_ig" {
-  vpc_id = aws_vpc.terraform_vpc.id
+resource "aws_internet_gateway" "ec2_ig" {
+  vpc_id = aws_vpc.ec2_vpc.id
 
   tags = {
     Name = "VPC_IGW"
   }
 }
 
-resource "aws_route_table" "terraform_public_rt" {
-  vpc_id = aws_vpc.terraform_vpc.id
+resource "aws_route_table" "ec2_public_rt" {
+  vpc_id = aws_vpc.ec2_vpc.id
 
-    route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.terraform_ig.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ec2_ig.id
   }
 
-    route {
-      ipv6_cidr_block = "::/0"
-      gateway_id = aws_internet_gateway.terraform_ig.id
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.ec2_ig.id
   }
 
-    tags = {
-      Name = "VPC_RT"
+  tags = {
+    Name = "VPC_RT"
   }
 }
 
-resource "aws_route_table_association" "terraform_public_1_rt_a" {
-  subnet_id      = aws_subnet.terraform_public_subnet.id
-  route_table_id = aws_route_table.terraform_public_rt.id
+resource "aws_route_table_association" "ec2_public_1_rt_a" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.ec2_public_rt.id
 }
 
 /* Defining firewall component */
-resource "aws_security_group" "terraform_sg" {
-  name   = "HTTP, HTTPS and SSH"
-  vpc_id = aws_vpc.terraform_vpc.id
+resource "aws_security_group" "ec2_sg" {
+  name        = "HTTP, HTTPS and SSH"
+  description = "EC2 security group"
+  vpc_id      = aws_vpc.ec2_vpc.id
 
   ingress {
     from_port   = 443
@@ -132,12 +132,12 @@ resource "aws_security_group" "terraform_sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = -1
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "My_SG"
-	  Description = "EC2 security group"
+    Name        = "My_SG"
+    Description = "EC2 security group"
   }
 }
