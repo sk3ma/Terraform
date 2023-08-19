2b0306c0439fee1401e9586b2fe4d9392cbc0458
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-08-19 21:00:56 +0200

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  value       = aws_instance.ec2.public_ip
-  description = "Public IP Address"
-}
-
-output "instance_id" {
-  value       = aws_instance.ec2.id
-  description = "Instance ID"
-}
