9bc4041c11810f878e6d036318bfb758b1a83366
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-08-19 22:33:29 +0200

    Update vars.tf

--- S3/vars.tf
+++ S3/vars.tf
@@ -5,6 +5,12 @@ variable "region" {
   default     = "eu-west-1"
 }
 
+variable "profile" {
+  description = "The AWS profile."
+  type        = string
+  default     = "playground"
+}
+
 variable "backup_bucket_name" {
   description = "Name of the S3 bucket for backups"
   type        = string
