bd99d75b19863fae61ceef2e7c00965cb72d6047
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-08-24 22:12:59 +0200

    Update rds.tf

--- Wordpress/rds.tf
+++ Wordpress/rds.tf
@@ -18,6 +18,6 @@ resource "aws_db_instance" "wpdb" {
   multi_az               = false
   storage_encrypted      = true
   db_subnet_group_name   = aws_db_subnet_group.mysql.name
-  vpc_security_group_ids = [aws_security_group._sg.id]
+  security_group_ids = [aws_security_group._sg.id]
   skip_final_snapshot    = true
 }
