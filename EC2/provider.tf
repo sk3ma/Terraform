8d6ac982d44080d25ca29256f87b0f494b3b341f
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-09-08 22:24:26 +0200

    Update provider.tf

--- EC2/provider.tf
+++ EC2/provider.tf
@@ -2,6 +2,15 @@
     TERRAFORM RESOURCE TO CREATE AN EC2 RESOURCE.
 */
 
+/* State file placement */
+#terraform {
+#  backend "s3" {
+#    bucket = "statebucket"
+#    key    = "infra"
+#    region = "eu-west-1"
+#  }
+#}
+
 /* Defining AWS provider */
 provider "aws" {
   profile = var.profile
