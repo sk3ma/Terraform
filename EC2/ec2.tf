f522b0390f4b9355091bc55e6c9aac102d68d1b4
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2024-04-17 11:59:48 +0200

    Updated file

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,22 +1,17 @@
+/* Defining virtual server */
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ssh_key"
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami           = var.ami
-  instance_type = var.instance
-  key_name      = aws_key_pair.ssh_key.key_name
-  subnet_id     = aws_subnet.subnet.id
+  ami                         = var.ami
+  instance_type               = var.instance
+  key_name                    = aws_key_pair.ssh_key.key_name
+  subnet_id                   = aws_subnet.subnet.id
+  associate_public_ip_address = true
   
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update
-              apt install software-properties-common  -y
-              apt-add-repository ppa:ansible/ansible -y
-              apt install ansible -y
-              EOF
-
+  user_data = templatefile("userdata.tpl")
   tags = {
     Name = "ec2"
   }
