bd7f1194357a7b24f31ad7890818062cc5992798
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2024-04-17 12:03:14 +0200

    Updated file

--- EC2/security.tf
+++ EC2/security.tf
@@ -1,24 +1,29 @@
 resource "aws_security_group" "sec_group" {
   name        = "ec2_security_group"
   description = "Allow inbound traffic on ports 80 and 443"
-
   vpc_id = aws_vpc.main.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = ["80, 443, 8080, 8443"]
+    content {
+      description = "Allow EC2 traffic"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_clocks = ["0.0.0.0/0"]
   }
+}
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    description = "Allow SSH traffic"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    cidr_clocks = ["10.0.0.0/16"]
+}
 
   egress {
+    description = "Allow all traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
