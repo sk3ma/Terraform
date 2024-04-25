e7afa0bcb4da0dd085433b3b1c80b1e0a5f76f14
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2024-04-25 10:29:47 +0200

    Updated file

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,3 +1,9 @@
+/* Defining SSH authentication */
+# resource "tls_private_key" "ssh_key" {
+#   algorithm = "RSA"
+#   rsa_bits  = 2048
+# }
+
 /* Defining virtual server */
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ssh_key"
@@ -10,7 +16,13 @@ resource "aws_instance" "ec2_instance" {
   key_name                    = aws_key_pair.ssh_key.key_name
   subnet_id                   = aws_subnet.subnet.id
   associate_public_ip_address = true
-  
+
+/* Defining storage volume */
+root_block_device {
+    volume_size = 10
+    volume_type = "gp2"
+  }
+
   user_data = templatefile("userdata.tpl")
   tags = {
     Name = "ec2"
