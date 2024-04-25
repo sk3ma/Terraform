02f402b3d563ebc9f04c441c59580dd52123ef89
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2024-04-25 18:00:57 +0200

    Updated file

--- EC2/provider.tf
+++ EC2/provider.tf
@@ -2,39 +2,12 @@
     TERRAFORM RESOURCE TO CREATE AN EC2 RESOURCE.
 */
 
-/* Defining Terraform provider */
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-/* State file placement */
-#terraform {
-#  backend "s3" {
-#    bucket = "statebucket"
-#    key    = "infra"
-#    region = "eu-west-1"
-#  }
-#}
-
-/* State file placement */
-# terraform {
-#   backend "remote" {
-#     organization = "<organization>"
-#     workspaces {
-#       name = "<workspace>"
-#     }
-#   }
-# }
-
 /* Defining AWS provider */
 provider "aws" {
 #  shared_credentials_file = "~/.aws/credentials"
   region                  = var.region
 #  profile                 = var.profile
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   alias                   = "ireland"
 }
