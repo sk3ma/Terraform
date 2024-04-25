de6c460f1a000c5f13de328165a6ac6b293c7c41
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2024-04-25 17:58:51 +0200

    Updated file

--- EC2/vars.tf
+++ EC2/vars.tf
@@ -1,3 +1,16 @@
+/* Defining resource variables */
+variable "AWS_ACCESS_KEY_ID" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
 variable "region" {
   type        = string
   description = "The AWS region"
