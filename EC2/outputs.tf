7961b5030c25a1449a30afffe7056020c271fc28
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2024-04-25 10:30:11 +0200

    Updated file

--- EC2/outputs.tf
+++ EC2/outputs.tf
@@ -1,7 +1,25 @@
+/* Defining resource outputs */
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "Region VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "secgroup_id" {
+  description = "Security Group ID"
+  value       = aws_security_group.sec_group.id
+}
+
+output "subnet_id" {
+  description = "EC2 subnet ID"
+  value       = aws_subnet.subnet.id
+}
+
+output "instance_id" {
+  description = "EC2 instance ID"
+  value       = aws_instance.ec2_instance.id
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.ec2_instance.public_ip
+  description = "EC2 instance IP"
+  value       = aws_instance.ec2_instance.public_ip
 }
